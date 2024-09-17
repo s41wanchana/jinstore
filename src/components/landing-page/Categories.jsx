e3ecab78df560defda2fbc4b1d1e51d098738ca7
Wanchana <s.wanchana41@gmail.com>
2024-09-18 03:46:35 +0700

    feat: categories nav first draft

--- src/components/landing-page/Categories.jsx
+++ src/components/landing-page/Categories.jsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+
 export default function Categories() {
-  return <h1>Categories</h1>;
+  return (
+    <div id="categories-nav-container">
+      <div id="fruit-n-vegies-nav" className="flex flex-col relative">
+        <div
+          id="circle"
+          className="bg-[#F3F4F6] rounded-full w-[116px] h-[116px] absolute -z-10"
+        ></div>
+        <Image
+          src="/images/fruit-n-vegies-category.png"
+          alt="fruit-category-img"
+          width={122}
+          height={122}
+        />
+        <p className="text-[13px] font-bold">Fruits & Vegetables</p>
+      </div>
+    </div>
+  );
 }
