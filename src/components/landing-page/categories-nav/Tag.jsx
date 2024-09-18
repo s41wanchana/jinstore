e83d19b36f4c9c3397c59576588d5d1520b7c54f
Wanchana <s.wanchana41@gmail.com>
2024-09-18 17:09:17 +0700

    fix:change tag component to button type

--- src/components/landing-page/categories-nav/Tag.jsx
+++ src/components/landing-page/categories-nav/Tag.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Tag({ id, imageUrl, text }) {
   return (
-    <div id={id} className="flex flex-col relative">
+    <button id={id} className="flex flex-col relative">
       <div className="bg-[#F3F4F6] rounded-full w-[116px] h-[116px] absolute -z-10"></div>
       <Image src={imageUrl} alt={`${id}-img`} width={122} height={122} />
       <p className="text-[13px] font-bold">{text}</p>
-    </div>
+    </button>
   );
 }
